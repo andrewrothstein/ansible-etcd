2178e24da12550a1de9d5b44f061c8f350c3012c
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-22 14:02:03 -0500

    up to v3.4.3

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,31 +1,38 @@
 #!/usr/bin/env sh
-VER=${1:-v3.4.2}
+#set -x
 DIR=~/Downloads
-MIRROR=https://github.com/etcd-io/etcd/releases/download/$VER
-RSHASUM=$MIRROR/SHA256SUMS
-LSHASUM=$DIR/etcd-${VER}-SHA256SUMS
-
-if [ ! -e $LSHASUM ];
-then
-    wget -q -O $LSHASUM $RSHASUM
-fi
+MIRROR=https://github.com/etcd-io/etcd/releases/download
 
 dl()
 {
-    local os=$1
-    local arch=$2
-    local archive_type=$3
+    local ver=$1
+    local lshasum=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
     local platform="${os}-${arch}"
-    local file=etcd-${VER}-${platform}.${archive_type}
-    local url=$MIRROR/$file
+    local file=etcd-${ver}-${platform}.${archive_type}
+    local url=$MIRROR/$ver/$file
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(grep $file $LSHASUM | awk '{print $1}')
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lshasum | awk '{print $1}')
 }
 
-printf "  # %s\n" $RSHASUM
-printf "  %s:\n" $VER
-dl darwin amd64 zip
-dl linux amd64 tar.gz
-dl linux arm64 tar.gz
-dl linux ppc64le tar.gz
-dl windows amd64 zip
+dl_ver() {
+    local ver=$1
+    local rshasum=$MIRROR/$ver/SHA256SUMS
+    local lshasum=$DIR/etcd-${ver}-SHA256SUMS
+
+    if [ ! -e $lshasum ]; then
+        wget -q -O $lshasum $rshasum
+    fi
+
+    printf "  # %s\n" $rshasum
+    printf "  %s:\n" $ver
+    dl $ver $lshasum darwin amd64 zip
+    dl $ver $lshasum linux amd64
+    dl $ver $lshasum linux arm64
+    dl $ver $lshasum linux ppc64le
+    dl $ver $lshasum windows amd64 zip
+}
+
+dl_ver ${1:-v3.4.3}
